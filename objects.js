9e0d3c1e14c0d9242c26881adf03b0941e1d5bf4
Muhammad Tahir Khan <muhammadtahirk@gmail.com>
2018-01-16 12:16:12 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playList = {
+  'Alan Walker': 'Faded',
+  'Sajjad Ali': 'Har Zulm'
+}
+
+Function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artist] = songTitle
+  return playlist
+}
+
+Function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
